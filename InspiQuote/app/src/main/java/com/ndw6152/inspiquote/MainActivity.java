3ee17276e962006414851ff7dffadc17b6dffe97
ndw6152 <ndwong19@gmail.com>
2018-04-28 18:27:02 -0700

    Minimizing keyboard when user click the search icon

--- InspiQuote/app/src/main/java/com/ndw6152/inspiquote/MainActivity.java
+++ InspiQuote/app/src/main/java/com/ndw6152/inspiquote/MainActivity.java
@@ -1,9 +1,11 @@
 package com.ndw6152.inspiquote;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -86,6 +88,12 @@ public class MainActivity extends AppCompatActivity {
         }
         else {
             RestClient.getRandomQuote(et_authorName.getText().toString(), updateScreenCallback);
+            View view1 = this.getCurrentFocus();
+            if (view1 != null) {
+                InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                assert imm != null;
+                imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+            }
         }
     }
 }
